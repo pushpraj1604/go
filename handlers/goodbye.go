584389d304ec5acf59e759324dae2b643b51c572
Pushpraj Gupta <pushprajbbdu@gmail.com>
2021-04-06 14:58:29 +0530

    handlers-goodbye

---
+++ handlers/goodbye.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodbye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (g *Goodbye) ServHTTP(rw http.ResponseWriter, r *http.request) {
+	rw.Write([]byte("bye
+}
